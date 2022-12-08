06505750061679cb727e5cc95bb80d017528f6de
manaye lingerew <113806279+manaye912@users.noreply.github.com>
2022-12-08 07:32:51 +0300

    file I/O

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int err, len, fd;
+
+	err = len = fd = 0;
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+		if (fd < 0)
+			return (-1);
+	while (text_content && text_content[len])
+		len++;
+
+	err = write(fd, text_content, len);
+	if (err < 0)
+		return (-1);
+	close(fd);
+	return (1);
+}
