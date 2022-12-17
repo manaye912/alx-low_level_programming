fb1c7da992cf5c820679f8f9840fa0b39c5d75d7
manaye912 <manayelingerew@gmail.com>
2022-12-17 03:22:18 -0800

    argv, argc

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,12 +4,12 @@
 #include <stdbool.h>
 
 /**
- * main -  prints the minimum number of coins to make 
+ * main -  prints the minimum number of coins to make
  * change for an amount of money.
- * @argc: number of command line argument 
+ * @argc: number of command line argument
  * @argv: array name
  * Return: 1 if not enough arguments passed in , 0 therwise
- * */
+ */
 
 int main(int argc, char *argv[])
 {
